a5e0379bcd492cece92fee801d66f71971235b88
sarthakKarode <sarthakkarode@gmail.com>
2025-08-04 22:19:29 +0530

    added eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import eslintPluginTs from '@typescript-eslint/eslint-plugin';
+import parserTs from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: parserTs,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': eslintPluginTs,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-inferrable-types': 'warn',
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+      'no-console': 'warn',
+      'eqeqeq': ['error', 'always'],
+      'curly': ['error', 'all'],
+      'prettier/prettier': 'error',
+    },
+  },
+];
